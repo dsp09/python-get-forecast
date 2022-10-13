a9524b6146d391713ee3563ee095c589ed376276
Devi Sai Prashanthi <34478827+dsp09@users.noreply.github.com>
2022-10-12 21:33:12 -0400

    Update script.py

--- script.py
+++ script.py
@@ -42,7 +42,7 @@ def get_forecast( city='Pittsburgh' ):
     forecast = forecast_response_json['properties']['periods']
     forecast_tonight = search_for_tonight(forecast)
 
-    return forecast_tonight[0]['startTime'], forecast_tonight[0]['endTime'], forecast_tonight[0]['detailedForecast']
+    return {'startTime':forecast_tonight[0]['startTime'], 'endTime':forecast_tonight[0]['endTime'], 'detailedForecast':forecast_tonight[0]['detailedForecast']}
     raise NotImplementedError()
 
 def main():
